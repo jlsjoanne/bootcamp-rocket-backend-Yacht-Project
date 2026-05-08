811778e8c2ad7a63033f4eb146443408cf411efd
Joanne H <jlsjoanne@gmail.com>
2026-05-08 17:22:53 +0800

    feat: add classes for Yacht, News, Dealer, and Contact in models

---
+++ Models/Dealer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TayanaYachts.Models
+{
+    public class Dealer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name="Dealer Name")]
+        public string Name { get; set; }
+
+        [AllowHtml]
+        public string Content { get; set; }
+
+        public UploadedFile Image { get;set; }
+
+        public virtual Area Area { get; set; }
+        
+    }
+}
---
+++ Models/UploadedFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TayanaYachts.Models
+{
+    public class UploadedFile
+    {
+        public Guid Id { get; set; }
+        
+        [Required]
+        public HttpPostedFileBase File { get;set; }
+
+        public string FileName { get; set; }
+    }
+}
