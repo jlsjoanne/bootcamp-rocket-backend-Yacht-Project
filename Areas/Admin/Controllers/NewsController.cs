ada1d6ccbf43170730395d60486492ee8be56821
Joanne H <jlsjoanne@gmail.com>
2026-06-12 18:02:41 +0800

    feat: add News controller and Index View

---
+++ Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TayanaYachts.DAL;
+using TayanaYachts.Models;
+using TayanaYachts.Models.ViewModels;
+
+namespace TayanaYachts.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class NewsController : Controller
+    {
+        private TayanaContext db = new TayanaContext();
+
+        // GET Admin/News
+        public ActionResult Index()
+        {
+            var news = db.News
+                .OrderByDescending(n => n.IsPinned)
+                .ThenByDescending(n => n.PublishDate);
+            return View(news.ToList());
+        }
+    }
+}
