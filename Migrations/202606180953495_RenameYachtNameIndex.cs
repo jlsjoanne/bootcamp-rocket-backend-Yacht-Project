743261f3b8ae0f4e24a16684d181efa0c94f9174
Joanne H <jlsjoanne@gmail.com>
2026-06-18 18:00:09 +0800

    feat: Test News Searching and paging

---
+++ Migrations/202606180953495_RenameYachtNameIndex.cs
@@ -0,0 +1,18 @@
+﻿namespace TayanaYachts.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameYachtNameIndex : DbMigration
+    {
+        public override void Up()
+        {
+            RenameIndex(table: "dbo.Yachts", name: "IX_Name", newName: "IX_Yacht_Name");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.Yachts", name: "IX_Yacht_Name", newName: "IX_Name");
+        }
+    }
+}
